781c199597d1f05bcb792dea2b7fe88622d98d88
Muhammad Bin Qasim <binqasimmuhammad2020@gmail.com>
2026-04-30 13:58:54 +0500

    Version 1.1 (Updated Version)

--- tk_calculator.py
+++ tk_calculator.py
@@ -1,9 +1,11 @@
 import tkinter as tk
 from tkinter import messagebox
+import math
 
 root = tk.Tk()
 root.title("Simple Calculator")
-root.geometry("350x300")
+root.geometry("350x380")
+
 
 def calculate(operation):
     try:
@@ -28,6 +30,26 @@ def calculate(operation):
         messagebox.showerror("Error", "Enter valid numbers!")
 
 
+def square():
+    try:
+        num = float(entry1.get())
+        result = num ** 2
+        result_label.config(text=f"Square: {result}")
+    except:
+        messagebox.showerror("Error", "Enter valid number in First box!")
+
+def square_root():
+    try:
+        num = float(entry1.get())
+        if num < 0:
+            messagebox.showerror("Error", "Negative number not allowed!")
+            return
+        result = math.sqrt(num)
+        result_label.config(text=f"Square Root: {result}")
+    except:
+        messagebox.showerror("Error", "Enter valid number in First box!")
+
+
 tk.Label(root, text="Enter First Number:").pack(pady=5)
 entry1 = tk.Entry(root)
 entry1.pack(pady=5)
@@ -40,6 +62,9 @@ tk.Button(root, text="Add (+)", width=12, command=lambda: calculate("add")).pack
 tk.Button(root, text="Subtract (-)", width=12, command=lambda: calculate("sub")).pack(pady=3)
 tk.Button(root, text="Multiply (×)", width=12, command=lambda: calculate("mul")).pack(pady=3)
 tk.Button(root, text="Divide (÷)", width=12, command=lambda: calculate("div")).pack(pady=3)
+
+tk.Button(root, text="Square (x²)", width=12, command=square).pack(pady=3)
+tk.Button(root, text="Square Root (√)", width=12, command=square_root).pack(pady=3)
 
 result_label = tk.Label(root, text="Result: ", font=("Arial", 12))
 result_label.pack(pady=10)
